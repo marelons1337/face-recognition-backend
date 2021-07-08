edc455083eb0eb681dba68c2b5c31869b76839da
rafal <jarorafo@gmail.com>
2021-07-08 23:38:12 +0200

    adding port

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/users/:id', (req, res) => { users.usersHandlerGet(req,res,db) })
 
 app.post('/register', (req, res) => { register.registerHandler(req, res, db, bcrypt, saltRounds) })
 
-app.listen(3000, ()=> {
-    console.log('running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`running on port ${ process.env.PORT }`);
 });
