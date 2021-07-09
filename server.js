194a1b8ec68f217c520f0e7d0d267938103bf780
rafal <jarorafo@gmail.com>
2021-07-09 14:54:36 +0200

    fixed connectivity

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const register = require('./controllers/register')
 const signin = require('./controllers/signin')
 const users = require('./controllers/users')
 const image = require('./controllers/image')
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = require('knex')({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
