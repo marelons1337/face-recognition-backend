5c843822df0c93413c26039e193898127841e0fe
rafal <jarorafo@gmail.com>
2021-07-09 14:41:47 +0200

    connection to heroku

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const image = require('./controllers/image')
 const db = require('knex')({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'rafal',
-      password : '',
-      database : 'face-recognition'
+      host : 'process.env.DATABASE_URL',
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
