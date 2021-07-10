2b69c26ec694b6920d7fef176829571a10c15bec
rafal <jarorafo@gmail.com>
2021-07-10 21:32:32 +0200

    fixing ssl

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const db = require('knex')({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false
-      }
+      ssl: false
     }
   });
 
